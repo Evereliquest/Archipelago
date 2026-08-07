5f544285fbaeb4e963724ddae9a13bc438fd47ca
Evereliquest <evereliquest@gmail.com>
2026-08-06 23:05:33 -0400

    Creation of files to move all the dicts out of the client

---
+++ worlds/teardown/client/locations.py
@@ -0,0 +1,633 @@
+
+Mission_Locations = {
+    "mall_intro": 1,
+    "lee_computers": 11,
+    "lee_login": 21,
+    "marina_demolish": 31,
+    "marina_cars": 41,
+    "mansion_pool": 61,
+    "lee_safe": 71,
+    "marina_gps": 51,
+    "lee_tower": 81,
+    "mansion_art": 91,
+    "marina_tools": 101,
+    "marina_art_back": 111,
+    "mall_foodcourt": 121,
+    "mansion_fraud": 131,
+    "caveisland_computers": 141,
+    "mansion_race": 151,
+    "mansion_safe": 161,
+    "lee_powerplant": 171,
+    "caveisland_propane": 181,
+    "caveisland_dishes": 191,
+    "lee_flooding": 201,
+    "frustrum_chase": 211,
+    "factory_espionage": 221,
+    "caveisland_ingredients": 231,
+    "frustrum_tornado": 241,
+    "mall_shipping": 251,
+    "carib_alarm": 261,
+    "carib_barrels": 271,
+    "carib_destroy": 281,
+    "carib_yacht": 291,
+    "frustrum_vehicle": 301,
+    "mall_decorations": 311,
+    "factory_tools": 321,
+    "mall_radiolink": 331,
+    "frustrum_pawnshop": 341,
+    "factory_robot": 351,
+    "lee_woonderland": 361,
+    "factory_explosive": 371,
+    "caveisland_roboclear": 381,
+    "cullington_bomb": 393,
+}
+
+Mission_Checked_Locations = {
+    "Old Building Problem": {
+        "prereq_id": 1,
+        "locations": [1]
+    },
+    "Lee Computers": {
+        "prereq_id": 2,
+        "locations": [11, 12, 13]
+    },
+    "Login Devices": {
+        "prereq_id": 3,
+        "locations": [21, 22, 23]
+    },
+    "Making Space": {
+        "prereq_id": 4,
+        "locations": [31, 32, 33]
+    },
+    "Classic Cars": {
+        "prereq_id": 5,
+        "locations": [41, 42, 43, 44]
+    },
+    "The GPS Devices": {
+        "prereq_id": 6,
+        "locations": [51, 52, 53, 54, 55]
+    },
+    "The Car Wash": {
+        "prereq_id": 7,
+        "locations": [61, 62, 63, 64, 65, 66]
+    },
+    "Heavy Lifting": {
+        "prereq_id": 8,
+        "locations": [71, 72, 73, 74, 75]
+    },
+    "The Tower": {
+        "prereq_id": 9,
+        "locations": [81]
+    },
+    "Fine Arts": {
+        "prereq_id": 10,
+        "locations": [91, 92, 93, 94, 95, 96]
+    },
+    "Tool Up": {
+        "prereq_id": 11,
+        "locations": [101, 102, 103, 104, 105, 106]
+    },
+    "Art Return": {
+        "prereq_id": 12,
+        "locations": [111, 112, 113, 114]
+    },
+    "Covert Chaos": {
+        "prereq_id": 13,
+        "locations": [121, 122, 123]
+    },
+    "Insurance Fraud": {
+        "prereq_id": 14,
+        "locations": [131, 132, 133, 134, 135, 136]
+    },
+    "The BlueTide Computers": {
+        "prereq_id": 15,
+        "locations": [141, 142, 143, 144, 145, 146, 147]
+    },
+    "The Speed Deal": {
+        "prereq_id": 16,
+        "locations": [151, 152, 153]
+    },
+    "A Wet Affair": {
+        "prereq_id": 17,
+        "locations": [161, 162, 163, 164, 165, 166]
+    },
+    "Power Outage": {
+        "prereq_id": 18,
+        "locations": [171, 172, 173, 174, 175, 176, 177, 178]
+    },
+    "Motivational Reminder": {
+        "prereq_id": 19,
+        "locations": [181, 182, 183, 184, 185, 186, 187, 188]
+    },
+    "An Assortment Of Dishes": {
+        "prereq_id": 20,
+        "locations": [191, 192, 193, 194, 195, 196, 197, 198, 199]
+    },
+    "Flooding": {
+        "prereq_id": 21,
+        "locations": [201, 202, 203, 204, 205, 206, 207, 208]
+    },
+    "The Chase": {
+        "prereq_id": 22,
+        "locations": [211]
+    },
+    "Roborazzi": {
+        "prereq_id": 23,
+        "locations": [221, 222, 223, 224, 225]
+    },
+    "The Secret Ingredients": {
+        "prereq_id": 24,
+        "locations": [231, 232, 233, 234, 235, 236]
+    },
+    "The BlueTide Shortage": {
+        "prereq_id": 25,
+        "locations": [241, 242, 243, 244, 245, 246]
+    },
+    "The Shipping Logs": {
+        "prereq_id": 26,
+        "locations": [251, 252, 253, 254, 255, 256, 257, 258]
+    },
+    "The Alarm System": {
+        "prereq_id": 27,
+        "locations": [261, 262, 263, 264, 265, 266]
+    },
+    "Moving The Goods": {
+        "prereq_id": 28,
+        "locations": [271, 272, 273, 274, 275]
+    },
+    "Havoc In Paradise": {
+        "prereq_id": 29,
+        "locations": [281, 282, 283, 284, 285, 286, 287]
+    },
+    "Elena's Revenge": {
+        "prereq_id": 30,
+        "locations": [291]
+    },
+    "Truckload Of Trouble": {
+        "prereq_id": 31,
+        "locations": [301, 302, 303]
+    },
+    "Ornament Ordeal": {
+        "prereq_id": 32,
+        "locations": [311, 312, 313, 314, 315, 316]
+    },
+    "The Quilez Tools": {
+        "prereq_id": 33,
+        "locations": [321, 322, 323, 324, 325, 326]
+    },
+    "Connecting The Dots": {
+        "prereq_id": 34,
+        "locations": [331, 332, 333, 334, 335]
+    },
+    "The Pawn Shop": {
+        "prereq_id": 35,
+        "locations": [341, 342, 343, 344, 345, 346, 347]
+    },
+    "The Droid Abduction": {
+        "prereq_id": 36,
+        "locations": [351, 352, 353, 354]
+    },
+    "Malice In Woonderland": {
+        "prereq_id": 37,
+        "locations": [361, 362, 363, 364, 365, 366, 367, 368]
+    },
+    "Handle With Care": {
+        "prereq_id": 38,
+        "locations": [371, 372, 373, 374, 375, 376, 377]
+    },
+    "Droid Dismount": {
+        "prereq_id": 39,
+        "locations": [381, 382, 383, 384, 385, 386, 387]
+    }
+}
+
+Tool_Locations = {
+    "toolupgrade/blowtorch/ammo": {
+        30: 501,
+        40: 502,
+        50: 503,
+        60: 504,
+    },
+    "toolupgrade/shotgun/ammo": {
+        24: 511,
+        36: 512,
+        48: 513,
+        60: 514,
+        72: 515,
+        84: 516,
+        96: 517,
+    },
+    "toolupgrade/shotgun/range": {
+        40: 521,
+        60: 522,
+    },
+    "toolupgrade/shotgun/damage": {
+        4: 531,
+        5: 532,
+    },
+    "toolupgrade/plank/ammo": {
+        16: 541,
+        24: 542,
+        32: 543,
+        40: 544,
+        48: 545,
+        56: 546,
+        64: 547,
+    },
+    "toolupgrade/plank/width": {
+        4: 551,
+        5: 552,
+    },
+    "toolupgrade/plank/length": {
+        48: 561,
+        56: 562,
+        64: 563,
+    },
+    "toolupgrade/pipebomb/ammo": {
+        12: 571,
+        18: 572,
+        25: 573,
+        30: 574,
+        36: 575,
+    },
+    "toolupgrade/pipebomb/damage": {
+        3: 581,
+        4: 582,
+    },
+    "toolupgrade/gun/ammo": {
+        12: 591,
+        18: 592,
+        24: 593,
+        30: 594,
+        36: 595,
+    },
+    "toolupgrade/gun/range": {
+        60: 601,
+        80: 602,
+        100: 603,
+    },
+    "toolupgrade/gun/damage": {
+        2: 611,
+        3: 612,
+    },
+    "toolupgrade/bomb/ammo": {
+        12: 621,
+        18: 622,
+        24: 623,
+        30: 624,
+        36: 625,
+    },
+    "toolupgrade/bomb/damage": {
+        5: 631,
+        6: 632,
+    },
+    "toolupgrade/rocket/ammo": {
+        12: 641,
+        18: 642,
+        24: 643,
+    },
+    "toolupgrade/rocket/damage": {
+        4: 651,
+        5: 652,
+    },
+    "toolupgrade/booster/ammo": {
+        12: 661,
+        18: 662,
+        24: 663,
+    },
+    "toolupgrade/booster/power": {
+        300: 671,
+        400: 672,
+    },
+    "toolupgrade/booster/time": {
+        6: 681,
+        8: 682,
+    },
+    "toolupgrade/leafblower/power": {
+        30: 691,
+        40: 692,
+        50: 693,
+    },
+    "toolupgrade/wire/ammo": {
+        12: 701,
+        18: 702,
+        24: 703,
+    },
+    "toolupgrade/wire/stretch": {
+        4: 711,
+        5: 712,
+    },
+    "toolupgrade/turbo/ammo": {
+        12: 721,
+        18: 722,
+        24: 723,
+    },
+    "toolupgrade/turbo/power": {
+        300: 731,
+        400: 732,
+    },
+    "toolupgrade/explosive/ammo": {
+        8: 741,
+        12: 742,
+        16: 743,
+    },
+    "toolupgrade/explosive/damage": {
+        6: 751,
+        7: 752,
+        8: 753,
+    },
+    "toolupgrade/rifle/ammo": {
+        12: 761,
+        18: 762,
+    },
+    "toolupgrade/steroid/ammo": {
+        3: 771,
+        4: 772,
+    },
+    "toolupgrade/steroid/time": {
+        5: 781,
+        6: 782,
+    },
+
+}
+
+Valuables_Names = [
+    "hub_banana",
+
+    "lee_trowel",
+    "lee_pneumaticwrench",
+    "lee_cash6",
+    "lee_college",
+    "lee_bits",
+    "lee_tilecutter",
+    "lee_disccutter",
+    "lee_wrench",
+    "lee_cutters",
+    "lee_cash1",
+    "lee_cash4",
+    "lee_cash3",
+    "lee_painting1",
+    "lee_painting2",
+    "lee_cash2",
+    "lee_pension",
+    "lee_painting4",
+    "lee_whisky",
+    "lee_circularsaw",
+    "lee_screwdriver",
+    "lee_bottles",
+    "lee_microscope",
+    "lee_cash5",
+    "lee_laser2",
+    "lee_wallet",
+    "lee_comics",
+    "lee_laser1",
+    "lee_stair_toolbox",
+    "lee_hammer",
+
+    "marina_cigarbox",
+    "marina_silvercoins",
+    "marina_cashbag",
+    "marina_sander",
+    "marina_amethyst",
+    "marina_callibration",
+    "marina_lubrication",
+    "marina_sonar",
+    "marina_sparkplugs",
+    "marina_propeller",
+    "marina_drill",
+    "marina_telescope",
+    "marina_gamingconsole",
+    "marina_sunglasses",
+    "marina_cashregister",
+    "marina_swordfish",
+    "marina_hook",
+    "marina_vacuum",
+    "marina_sword",
+    "marina_dagger",
+    "marina_modelship",
+    "marina_fishing",
+    "marina_mp3",
+    "marina_binoculars",
+    "marina_compass",
+    "marina_cannonball",
+    "marina_cashbox",
+    "marina_trophy",
+    "marina_gun",
+    "marina_painting1",
+    "marina_flashlight",
+    "marina_walkietalkie",
+    "marina_sextant",
+    "marina_tequila",
+    "marina_lifevest",
+    "marina_steeringwheel",
+
+    "mansion_wallet1",
+    "liquor1",
+    "mansion_passport",
+    "mansion_soup",
+    "mansion_walkman",
+    "vacuumcleaner",
+    "mansion_gift",
+    "mansion_knives",
+    "mansion_caviar",
+    "mansion_pan",
+    "mansion_elevatormanual",
+    "mansion_cash",
+    "mansion_coincollection",
+    "mansion_bronzestatue",
+    "mansion_remote",
+    "mansion_cablebox",
+    "mansion_dictaphone",
+    "mansion_snookerbook",
+    "mansion_popcornmanual",
+    "mansion_moviebook",
+    "mansion_wallet3",
+    "mansion_sneakers",
+    "mansion_tshirt",
+    "mansion_lighter2",
+    "mansion_creditcard",
+    "mansion_gardeningbook",
+    "mansion_makeup",
+    "mansion_pills",
+    "mansion_wallet2",
+    "mansion_silverware",
+    "mansion_oysters",
+    "mansion_foodprocessor",
+    "guesthouse_lamp",
+    "mansion_jewelry",
+    "mansion_charcoal",
+    "mansion_trophy",
+    "mansion_lighter",
+    "mansion_chocolate",
+    "mansion_wine",
+    "mansion_toiletbrush",
+    "mansion_thermometer",
+    "mansion_calibration",
+    "mansion_carburetor",
+    "mansion_drill",
+    "mansion_model_train",
+    "fraud_toolbox",
+    "mansion_polishtrophy",
+
+    "caveisland_phone",
+    "caveisland_disccutter",
+    "caveisland_drill",
+    "caveisland_detergent",
+    "caveisland_nutrition",
+    "caveisland_yeast",
+    "caveisland_pesticide",
+    "caveisland_oldtv",
+    "caveisland_crock-of-gold",
+    "caveisland_carbon_lamp",
+    "caveisland_tar",
+    "caveisland_fishinggear",
+    "caveisland_fishingscale",
+    "caveisland_radio",
+    "caveisland_labeling",
+    "caveisland_fishingknife",
+    "caveisland_cashregister1",
+    "caveisland_btextra",
+    "caveisland_pulsewatch",
+    "caveisland_cashregister2",
+    "caveisland_gin",
+    "caveisland_monitor",
+    "caveisland_book",
+    "caveisland_alarmclock1",
+    "caveisland_tv1",
+    "caveisland_wallet1",
+    "caveisland_chess",
+    "caveisland_vacuum",
+    "caveisland_projector",
+    "caveisland_binoculars",
+    "caveisland_gardenscissors",
+    "caveisland_ragswater",
+    "caveisland_drilltape",
+    "caveisland_wine",
+    "caveisland_airpurifier",
+    "caveisland_pills",
+    "caveisland_vaultgold",
+    "caveisland_vaultcash",
+
+    "mall_durablephone",
+    "mall_taco",
+    "mall_busybooks",
+    "mall_flashlight",
+    "mall_trufflejuice",
+    "mall_bluetidecashbox",
+    "mall_furcoat",
+    "mall_democash",
+    "mall_safecracking",
+    "mall_pinkspray",
+    "mall_wallet1",
+    "mall_hoodie",
+    "mall_goldwatch",
+    "mall_necklace",
+    "mall_tiepin",
+    "mall_stapler",
+    "mall_skateboard",
+    "mall_civet",
+    "mall_cash1",
+    "mall_vram",
+    "mall_vodka",
+    "mall_gardenscissors",
+    "mall_underwear",
+    "mall_fxpedal",
+    "mall_radio",
+    "mall_vinegar",
+    "mall_eaudetoilette",
+
+    "frustrum_oilpaint",
+    "frustrum_itbonus",
+    "frustrum_oil",
+    "frustrum_wallet1",
+    "frustrum_reciprocating",
+    "frustrum_lp",
+    "frustrum_shaver",
+    "frustrum_fredaward",
+    "frustrum_tribalmask",
+    "frustrum_darts",
+    "frustrum_smokemachine",
+    "frustrum_wallet3",
+    "frustrum_dehumidifier",
+    "frustrum_bones",
+    "frustrum_fabricsoftener",
+    "frustrum_wallet2",
+    "frustrum_spices",
+    "frustrum_lotion",
+    "frustrum_filter",
+    "frustrum_ring",
+    "frustrum_lure",
+    "frustrum_wallet4",
+    "frustrum_hat",
+    "frustrum_guitar",
+    "frustrum_preasuremeter",
+    "frustrum_bits",
+    "frustrum_harmonica",
+
+    "factory_bulletproof",
+    "factory_microphone",
+    "factory_aicore",
+    "factory_gyroscope",
+    "factory_gearmotor",
+    "factory_radio",
+    "factory_oldoutboardmotor",
+    "factory_infraredtransmitter",
+    "factory_semiconductors",
+    "factory_campingguide",
+    "factory_climbinghat",
+    "factory_roses",
+    "factory_umbrella",
+    "factory_key",
+    "factory_cigars",
+    "factory_gillianbribe",
+    "factory_distancesensor",
+    "factory_cards",
+    "factory_waterproof",
+    "factory_roboteye",
+    "factory_survivalbook",
+    "factory_lightsensor",
+    "factory_servo",
+    "factory_ultrasonicsensor",
+    "factory_infraredsensor",
+    "factory_scubagear",
+    "factory_watersensor",
+    "factory_fishingrod",
+    "factory_helicoptermanual",
+    "factory_explosionproof",
+
+    "carib_snorkel",
+    "carib_volleyball",
+    "carib_exoticfruit",
+    "carib_rims",
+    "carib_knuckles",
+    "carib_wallet",
+    "carib_monkeyhand",
+    "carib_moneycounter",
+    "carib_diamondcane",
+    "carib_islandlife",
+    "carib_teapot",
+    "carib_grease",
+    "carib_copperwire",
+    "carib_jetskiengine",
+    "carib_grappa",
+    "carib_oldmagazines",
+    "carib_skate",
+    "carib_lastroll",
+    "carib_chainsaw",
+    "carib_carburetor",
+    "carib_pegleg",
+    "carib_supercharger",
+    "carib_messageinabottle",
+    "carib_scale",
+    "carib_tuningkit",
+    "carib_tropicalhelmet",
+    "carib_goldenbullets",
+    "carib_scubatank",
+    "carib_birdegg",
+    "carib_shell",
+    "carib_goldengrillz",
+    "carib_pineapple",
+    "carib_duck",
+    "carib_bayran",
+
+]
